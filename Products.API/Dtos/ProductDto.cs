f95fbefcb1791bba596d19e4590e5452bf720d43
Asphyxia <andresi28@hotmail.es>
2025-06-14 22:56:03 -0600

    Configure and finalize Products API in Program.cs

--- Products.API/Dtos/ProductDto.cs
+++ Products.API/Dtos/ProductDto.cs
@@ -8,6 +8,4 @@ public class ProductDto
     public string? Description { get; set; }
     public int Id { get; set; }
     public decimal Price { get; set; }
-    public string? Category { get; set; }
-    public string? Image { get; set; }
 }
--- Products.API/Program.cs
+++ Products.API/Program.cs
@@ -1,41 +1,37 @@
+// Web application Builder.
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Services necessary for the controllers to function.
+builder.Services.AddControllers();
+ 
+// Adds services from Swagger/OpenAPI documentation.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Initializes the HttpClient to connect with the Platform.sh API.
+[cite_start]
+builder.Services.AddHttpClient("PlatformSH", client =>
+{
+    client.BaseAddress = new Uri("https://api.platform.sh/api/projects/pj2g557uk2z2g/");
+});
+
+// Builder 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP request pipeline.
+
+// Enables the swagger and UI.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// Redirects HTTP requests,
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Defines controller paths for API use.
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
