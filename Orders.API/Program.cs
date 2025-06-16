bd1128be4d59f6c4659d95635f5c5ea267bbeffd
Asphyxia <andresi28@hotmail.es>
2025-06-16 00:16:27 -0600

    docs: Add Postman collection for API testing

--- Orders.API/Program.cs
+++ Orders.API/Program.cs
@@ -20,7 +20,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 // Redirects HTTP requests,
 app.UseHttpsRedirection();
 
--- Products.API/ProductsController.cs
+++ Products.API/ProductsController.cs
@@ -5,7 +5,7 @@ namespace Products.API.Controllers
 { 
 
     //Attributes are required for driver discovery and operation.Finalize Products API setup in Program.cs.
-    
+
     [ApiController]
 
     [Route("api/[controller]")] 
